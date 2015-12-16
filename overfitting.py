75c38479b268c7bef862e3fc84b918214e7b9578
Ryan Kuhle <ryan@ryankuhle.com>
2015-12-16 13:29:29 -0600

    Plot training data with linear regression.. Plot  testing  data with  linear regression.

--- overfitting.py
+++ overfitting.py
@@ -1,6 +1,7 @@
 import numpy as np
 import statsmodels.formula.api as smf
 import pandas as pd
+import matplotlib.pyplot as plt
 
 np.random.seed(414) # create seed based on 414 starting point
 
@@ -16,7 +17,17 @@ test_X, test_y = X[700:], y[700:]
 train_df = pd.DataFrame({'X': train_X, 'y': train_y})
 test_df = pd.DataFrame({'X': test_X, 'y': test_y})
 
-train_df.plot(style=['o','rx'])
+# plot training data with linear regression
+m, b = np.polyfit(train_df['X'], train_df['y'], 1)
+plt.plot(train_df['X'], train_df['y'], '.')
+plt.plot(train_df['X'], m*train_df['X'] + b, '-')
+plt.show()
+
+# plot testing data with linear regression
+m, b = np.polyfit(test_df['X'], test_df['y'], 1)
+plt.plot(test_df['X'], test_df['y'], '.')
+plt.plot(test_df['X'], m*test_df['X'] + b, '-')
+plt.show()
 
 # linear fit
 poly_1 = smf.ols(formula='y ~ 1 + X', data=train_df).fit()
